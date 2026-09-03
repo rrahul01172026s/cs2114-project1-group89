c3819ca635b9a99d6e891fdf7049f3ddd75d3098
rrahul01172026s <rrahul0117@vt.edu>
2026-09-03 19:23:15 -0400

    Create Rahul.java

---
+++ Rahul.java
@@ -0,0 +1,13 @@
+public class Rahul
+{
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+   
+        public static void main(String[] args) {
+            System.out.println("Rahul Rajendran is on the team.");
+        }
+    
+    //~Public  Methods ........................................................
+
+}
