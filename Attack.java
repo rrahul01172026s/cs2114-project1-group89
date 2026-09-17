ef04225ea285ae9cd8965e867bb6e4bb41165dec
rrahul01172026s <rrahul0117@vt.edu>
2026-09-17 17:20:13 -0400

    Update Attack.java

--- Attack.java
+++ Attack.java
@@ -4,8 +4,9 @@ public class Attack
     private final String name;
     private final int power;
     private final BeastType type;
+    private final int staminaCost;
     //~ Constructors ..........................................................
-    public Attack(String name, int power, BeastType type) {
+    public Attack(String name, int power, BeastType type, int staminaCost) {
         if (name == null) {
             throw new IllegalArgumentException("Attack name cannot be null");
         }
@@ -16,22 +17,50 @@ public class Attack
         if (type == null) {
             throw new IllegalArgumentException("Attack type cannot be null");
         }
+        if (staminaCost < 0) {
+            throw new IllegalArgumentException("Stamina cost cannot be less" 
+                + " than 0");
+        }
         this.name = name;
         this.power = power;
         this.type = type;
+        this.staminaCost = staminaCost;
+        
+        
     }
     
    
     //~Public  Methods ........................................................
+    /**
+     * @return the move's name
+     */
     public String getName() {
         return this.name;
     }
-    
+    /**
+     * @return the moves power
+     */
     public int getPower() {
         return this.power;
     }
-    
+    /**
+     * @return the moves type
+     */
     public BeastType getType() {
         return this.type;
+    }
+    /**
+     * @return the move's stamina cost
+     */
+    public int getStaminaCost() {
+        return this.staminaCost;
+    }
+    /**
+     * @return a menu label for the attack
+     */
+    @Override
+    public String toString() {
+        return name + " (" + type + ", Power " + power + ", Cost " 
+            + staminaCost + ")";
     }
 }
