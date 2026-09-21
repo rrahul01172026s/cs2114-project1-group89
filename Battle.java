6caaef4435e429fb5689a596cb7cfe64d0b29799
kylev08 <kylodoggy@gmail.com>
2026-09-21 16:54:52 -0400

    Created DamageCalculator
    Battle is mostly fleshed out. Also adjusted Battle and BattleTest to reflect new class implementations.

--- Battle.java
+++ Battle.java
@@ -1,5 +1,5 @@
 /**
- * Handles turn-based combat
+ * Handles the turn-based combat
  * 
  * @author kvasallo
  * @version Sep 17, 2026
@@ -10,59 +10,56 @@ public class Battle {
     private static final int REST_STAMINA_RECOVERED = 7;
 
     // String types are placeholders for when the actual classes are built
-    private String playerBeast;
-    private String opponent;
-    private String opponentBeast;
-    private String inputHandler;
-    private String display;
+    private Beast playerBeast;
+    private String opponent; // TODO implement when opponent class is created
+    private Beast opponentBeast;
+    private InputHandler inputHandler;
+    private Display display;
+    private DamageCalculator calculator;
     private int turnNumber;
-    private String calculator;
+
+    private boolean playerFirst;
 
     // ~ Constructors ..........................................................
     /**
      * Create a new Battle object.
-     * @param playerBeast 
-     * @param opponent 
-     * @param opponentBeast 
-     * @param inputHandler 
-     * @param display 
-     * @param calculator 
+     * 
+     * @param playerBeast
+     *            The player's Beast
+     * @param opponent
+     *            The current opponent being faced
+     * @param opponentBeast
+     *            The Beast of the opponent
+     * @param inputHandler
+     *            The inputHandler
+     * @param display
+     *            The Display
+     * @param calculator
+     *            The calculator
+     * @throws IllegalArgumentException
+     *             When an argument is null
      */
     public Battle(
-        String playerBeast,
+        Beast playerBeast,
         String opponent,
-        String opponentBeast,
-        String inputHandler,
-        String display,
-        String calculator) {
-        
+        Beast opponentBeast,
+        InputHandler inputHandler,
+        Display display,
+        DamageCalculator calculator) {
+
+        if (playerBeast == null || opponent == null || opponentBeast == null
+            || inputHandler == null || display == null || calculator == null) {
+            throw new IllegalArgumentException();
+        }
+
         this.playerBeast = playerBeast;
         this.opponent = opponent;
         this.opponentBeast = opponentBeast;
         this.inputHandler = inputHandler;
         this.display = display;
         this.calculator = calculator;
-
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Adds dialogue and continually calls playTurn until a victor is determined
-     * 
-     * @return 
-     */
-    // ~Public Methods ........................................................
-    public boolean run() {
-        //false is returned for now to resolve compilation error
-        return false;
-    }
-
-
-    /**
-     * Runs through a single turn of a participant
-     */
-    private void playTurn() {
+        turnNumber = 0;
+        determineTurnOrder();
 
     }
 
@@ -76,4 +73,130 @@ public class Battle {
     public int getTurnNumber() {
         return turnNumber;
     }
+
+
+    /**
+     * Checks if the current battle is over
+     * 
+     * @return true if any beast fainted
+     */
+    private boolean isOver() {
+        return playerBeast.isFainted() || opponentBeast.isFainted();
+    }
+    
+    public BattleResult getResult() {
+        if (opponentBeast.isFainted()) {
+            return BattleResult.PLAYER_WON;
+        }
+        if (playerBeast.isFainted()) {
+            return BattleResult.PLAYER_LOST;
+        }
+        throw new IllegalStateException("Battle is not over yet");
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Adds dialogue and continually calls playTurn until a victor is determined
+     * 
+     * @return battle result PLAYER_WON or PLAYER_LOST either player's beast
+     *         fainted or the
+     *         opponent beast fainted
+     */
+    // ~Public Methods ........................................................
+    public BattleResult run() {
+        System.out.println("Opponent intro line");
+        while (!isOver()) {
+            playTurn();
+        }
+        BattleResult result = getResult();
+        if (result == BattleResult.PLAYER_WON) {
+            display.showBattleWon(opponentBeast);
+            //TODO opponent lose line implementation
+        }
+        else {
+            display.showBattleLost(playerBeast);
+            //TODO opponent win line implementation
+        }
+        
+        return result;
+    }
+
+
+    /**
+     * Runs through a single turn of a participant
+     */
+    private void playTurn() {
+        if (isOver()) {
+            throw new IllegalStateException();
+        }
+        if (playerFirst) {
+            display.showStatus(playerBeast, opponentBeast);
+            Attack chosenAttack = choosePlayerAttack();
+            if (chosenAttack == null) {
+                rest(playerBeast);
+            }
+            else {
+                useAttack(playerBeast, opponentBeast, chosenAttack);
+            }
+        }
+
+        if (!opponentBeast.isFainted()) {
+            // placeholder behavior for opponent class
+            rest(opponentBeast);
+        }
+
+        if (!playerBeast.isFainted()) {
+            playerBeast.recoverStamina(STAMINA_REGEN);
+        }
+
+        if (!opponentBeast.isFainted()) {
+            opponentBeast.recoverStamina(STAMINA_REGEN);
+        }
+
+    }
+
+
+    private Attack choosePlayerAttack() {
+        int restOption = playerBeast.getAttackCount() + 1;
+        while (true) {
+            int choice = inputHandler.readChoice("Choose move", 1, restOption);
+            if (choice == restOption) {
+                return null;
+            }
+
+            Attack attackChosen = playerBeast.getAttack(choice - 1);
+            if (playerBeast.canUse(attackChosen)) {
+                return attackChosen;
+            }
+            display.showError("pick a different move");
+        }
+    }
+
+
+    private void rest(Beast beast) {
+        beast.recoverStamina(REST_STAMINA_RECOVERED);
+        display.showRest(beast, REST_STAMINA_RECOVERED);
+    }
+
+
+    private void determineTurnOrder() {
+        if (playerBeast.getSpeed() >= opponentBeast.getSpeed()) {
+            playerFirst = true;
+        }
+        else {
+            playerFirst = false;
+        }
+    }
+
+
+    private void useAttack(Beast attacker, Beast defender, Attack attack) {
+        attacker.useStamina(attack.getStaminaCost());
+        int damage = calculator.calculateDamage(attack, defender);
+        defender.takeDamage(damage);
+        display.showAttack(attacker, attack, damage);
+        display.showEffectiveness(calculator.getEffectiveness(attack.getType(),
+            defender.getType()));
+    }
+
 }
--- BattleTest.java
+++ BattleTest.java
@@ -1,38 +1,56 @@
+import java.util.Scanner;
 import student.TestCase;
 
 /**
- *  Tests the Battle class
+ * Tests the Battle class
  * 
- *  @author kvasallo
- *  @version Sep 17, 2026
+ * @author kvasallo
+ * @version Sep 17, 2026
  */
 public class BattleTest extends TestCase {
-    //~ Fields ................................................................
+    // ~ Fields ................................................................
     private Battle battle;
-    
-    private String PLAYER_BEAST = "The player's beast";
-    private String OPPONENT = "The opponent the play is facing";
-    private String OPPONENT_BEAST = "The beast the opponent has";
-    private String INPUT_HANDLER = "Handles input";
-    private String DISPLAY = "Displays stuff";
-    private String CALCULATOR = "Calculates stuff";
 
-    //~Public  Methods ........................................................
+    private Attack[] ATTACKS = { new Attack("Attack 1", 1, BeastType.FIRE, 1),
+        new Attack("Attack 2", 2, BeastType.WATER, 1), new Attack("Attack 3", 3,
+            BeastType.GRASS, 1), new Attack("Attack 4", 4, BeastType.NORMAL,
+                1) };
+
+    private Beast PLAYER_BEAST = new Beast("player beast", BeastType.GRASS, 100,
+        100, 100, ATTACKS);
+    
+    private String OPPONENT = "The opponent the player is facing";
+    
+    private Beast OPPONENT_BEAST = new Beast("opp beast", BeastType.FIRE, 100,
+        100, 1, ATTACKS);
+    
+    private Display DISPLAY = new Display(System.out);
+    
+    private InputHandler INPUT_HANDLER = new InputHandler(new Scanner("ph"),
+        DISPLAY);
+
+    private DamageCalculator CALCULATOR = new DamageCalculator();
+
+    // ~Public Methods ........................................................
     public void setUp() {
-        battle = new Battle(PLAYER_BEAST, OPPONENT, OPPONENT_BEAST, INPUT_HANDLER, DISPLAY, CALCULATOR);
+        battle = new Battle(PLAYER_BEAST, OPPONENT, OPPONENT_BEAST,
+            INPUT_HANDLER, DISPLAY, CALCULATOR);
     }
+
 
     /**
      * Test run method
      */
     public void testRun() {
         
+
     }
+
 
     /**
      * Tests getNumberTurn method
      */
     public void testGetNumberTurn() {
-        
+
     }
 }
