ee6cb551f1469258050c73adfbbe182d89917233
rrahul01172026s <rrahul0117@vt.edu>
2026-09-22 16:53:07 -0400

    Fixing formatting

--- Beast.java
+++ Beast.java
@@ -1,19 +1,17 @@
 /**
  * // -------------------------------------------------------------------------
-/**
- *  One creature in the game. A beast has a name, and elemental type, health
- *  stamina, and 4 attacks
- *  
- *  Current health and stamina are the only values that can change during a battle
+ * /** One creature in the game. A beast has a name, and elemental type, health
+ * stamina, and 4 attacks Current health and stamina are the only values that
+ * can change during a battle
  * 
- *  @author rrahu
- *  @version Sep 17, 2026
+ * @author rrahu
+ * @version Sep 17, 2026
  */
 public class Beast
 {
-    //~ Fields ................................................................
+    // ~ Fields ................................................................
     private static final int ATTACK_COUNT = 4;
-    
+
     private final String name;
     private final BeastType type;
     private final int maxHealth;
@@ -22,31 +20,45 @@ public class Beast
     private final Attack[] attacks;
     private int currentHealth;
     private int currentStamina;
-    
-    //~ Constructors ..........................................................
+
+    // ~ Constructors ..........................................................
     /**
      * Creates a beast at full input and stamina
      */
-    public Beast(String name, BeastType type, int maxHealth, int maxStamina 
-           , int speed, Attack[] attacks) {
-        if (name == null || name.isBlank()) {
+    public Beast(
+        String name,
+        BeastType type,
+        int maxHealth,
+        int maxStamina,
+        int speed,
+        Attack[] attacks)
+    {
+        if (name == null || name.isBlank())
+        {
             throw new IllegalArgumentException("Beast name cannot be blank");
         }
-        if (type == null) {
+        if (type == null)
+        {
             throw new IllegalArgumentException("Type cannot be null");
         }
-        if (maxHealth <= 0) {
+        if (maxHealth <= 0)
+        {
             throw new IllegalArgumentException("Health cannot be less than 0");
         }
-        if (maxStamina <= 0) {
+        if (maxStamina <= 0)
+        {
             throw new IllegalArgumentException("Stamina cannot be less than 0");
         }
-        if (speed <= 0) {
+        if (speed <= 0)
+        {
             throw new IllegalArgumentException("Speed cannot be less than 0");
         }
-        for (Attack a : attacks) {
-            if (a == null) {
-                throw new IllegalArgumentException("Attacks cannot contain null");
+        for (Attack a : attacks)
+        {
+            if (a == null)
+            {
+                throw new IllegalArgumentException(
+                    "Attacks cannot contain null");
             }
         }
         this.name = name;
@@ -58,111 +70,185 @@ public class Beast
         this.currentStamina = maxStamina;
         this.speed = speed;
     }
-    //~Public  Methods ........................................................
+
+
+    // ~Public Methods ........................................................
     /** @return the Beast's name */
-    public String getName() { return name; }
+    public String getName()
+    {
+        return name;
+    }
+
+
     /** @return the Beast's elemental type */
-    public BeastType getType() { return type; }
+    public BeastType getType()
+    {
+        return type;
+    }
+
+
     /** @return current health, always 0 to maxHealth */
-    public int getCurrentHealth() { return currentHealth; }
+    public int getCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+
     /** @return maximum health */
-    public int getMaxHealth() { return maxHealth; }
+    public int getMaxHealth()
+    {
+        return maxHealth;
+    }
+
+
     /** @return current stamina, always 0 to maxStamina */
-    public int getCurrentStamina() { return currentStamina; }
+    public int getCurrentStamina()
+    {
+        return currentStamina;
+    }
+
+
     /** @return maximum stamina */
-    public int getMaxStamina() { return maxStamina; }
-    
+    public int getMaxStamina()
+    {
+        return maxStamina;
+    }
+
+
     /**
-     * @param index a 0 based position from 0-3
+     * @param index
+     *            a 0 based position from 0-3
      * @return the attack at that position
-     * @throws IndexOutofBoundsException if index is not 0-3
-     * 
+     * @throws IndexOutofBoundsException
+     *             if index is not 0-3
      */
-    public Attack getAttack(int index) {
-        if (index < 0 || index >= ATTACK_COUNT) {
+    public Attack getAttack(int index)
+    {
+        if (index < 0 || index >= ATTACK_COUNT)
+        {
             throw new IndexOutOfBoundsException("Attack must be be 0-3");
         }
         return attacks[index];
     }
+
+
     /**
      * @return speed of the beast
      */
-    public int getSpeed() {
+    public int getSpeed()
+    {
         return this.speed;
     }
+
+
     /**
      * @return 4, the number of attacks every Beast has
      */
-    public int getAttackCount() {
+    public int getAttackCount()
+    {
         return ATTACK_COUNT;
     }
+
+
     /**
      * Lowers health by the given amount, stopping at 0
      * 
-     * @param amount damage to take; cannot be negative
-     * @throws IllegalArgumentException if amount is negative
+     * @param amount
+     *            damage to take; cannot be negative
+     * @throws IllegalArgumentException
+     *             if amount is negative
      */
-    public void takeDamage(int amount) {
-        if (amount < 0) {
+    public void takeDamage(int amount)
+    {
+        if (amount < 0)
+        {
             throw new IllegalArgumentException("Amount cannot be less than 0");
         }
         currentHealth = Math.max(0, currentHealth - amount);
     }
+
+
     /**
      * @return true when health has reached 0
      */
-    public boolean isFainted() {
+    public boolean isFainted()
+    {
         return currentHealth == 0;
     }
-    public boolean canUse(Attack attack) {
-        if (attack == null) {
+
+
+    public boolean canUse(Attack attack)
+    {
+        if (attack == null)
+        {
             throw new IllegalArgumentException("Attack cannot be null");
         }
         return currentStamina >= attack.getStaminaCost();
     }
+
+
     /**
      * Spends stamina on a move.
      *
-     * @param amount stamina to spend; cannot be negative
-     * @throws IllegalArgumentException if amount is negative
-     * @throws IllegalStateException if there is not enough stamina
+     * @param amount
+     *            stamina to spend; cannot be negative
+     * @throws IllegalArgumentException
+     *             if amount is negative
+     * @throws IllegalStateException
+     *             if there is not enough stamina
      */
-    public void useStamina(int amount) {
-        if (amount < 0) {
-            throw new IllegalArgumentException("Stamina amount cannot be negative");
+    public void useStamina(int amount)
+    {
+        if (amount < 0)
+        {
+            throw new IllegalArgumentException(
+                "Stamina amount cannot be negative");
         }
-        if (amount > currentStamina) {
+        if (amount > currentStamina)
+        {
             throw new IllegalStateException("Not enough stamina");
         }
         currentStamina -= amount;
     }
-    
+
+
     /**
      * Adds stamina, never going above the maximum.
      *
-     * @param amount stamina to add; cannot be negative
-     * @throws IllegalArgumentException if amount is negative
+     * @param amount
+     *            stamina to add; cannot be negative
+     * @throws IllegalArgumentException
+     *             if amount is negative
      */
-    public void recoverStamina(int amount) {
-        if (amount < 0) {
-            throw new IllegalArgumentException("Stamina amount cannot be negative");
+    public void recoverStamina(int amount)
+    {
+        if (amount < 0)
+        {
+            throw new IllegalArgumentException(
+                "Stamina amount cannot be negative");
         }
         currentStamina = Math.min(maxStamina, currentStamina + amount);
     }
+
+
     /**
      * Refills health and stamina to max
      */
-    public void restoreAll() {
+    public void restoreAll()
+    {
         currentHealth = maxHealth;
         currentStamina = maxStamina;
     }
+
+
     /**
      * @return a status line for the beast
      */
     @Override
-    public String toString() {
+    public String toString()
+    {
         return name + " [" + type + "] HP " + currentHealth + "/" + maxHealth
             + " SP " + currentStamina + "/" + maxStamina;
     }
-    
+
 }
